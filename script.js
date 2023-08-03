772a6962a2bc29644d74a5e63e7e5ff669d3e538
typeonwood <typeonwood@gmail.com>
2023-08-02 23:59:49 -0700

    divide and rewrite functions

--- script.js
+++ script.js
@@ -27,13 +27,33 @@ function operate(num1, operator, num2) {
     }
 }
 
-let input = []
+let input = ['0']
 let displayText = ''
+
+function inputUpdate(start, end, answer) {
+    let inputString = input.join(',');
+    let expression = inputString.slice(start, end + 1);
+    inputString = inputString.replace(expression, answer);
+    input = inputString.split(',')
+}
+
+function displayUpdate() {
+    let inputString = input.join('');
+    displayText = inputString.replaceAll('**', '^').
+                              replaceAll('sqrt', '√').
+                              replaceAll('*', ' × ').
+                              replaceAll('/', ' ÷ ').
+                              replaceAll('+', ' + ').
+                              replaceAll('-', ' - ');
+    const display = document.querySelector('.display');
+    display.textContent = displayText;
+}
 
 function displayInput(e) {
     let id = e.target.id
     let item
     let last = input.length - 1;
+    if (input.length == 1 && input[0] == 0) input = [];
     if (input.join('').length > 16) return 'too many digits'
     else if (isFinite(Number(id)) || id === '.') {
         if (input[last] !== undefined) {
@@ -46,15 +66,7 @@ function displayInput(e) {
         } else input.push(`${id}`);
     } 
     else input.push(`${id}`);
-    let inputString = input.join('');
-    displayText = inputString.replaceAll('**', '^').
-                              replaceAll('sqrt', '√').
-                              replaceAll('*', ' × ').
-                              replaceAll('/', ' ÷ ').
-                              replaceAll('+', ' + ').
-                              replaceAll('-', ' - ');
-    const display = document.querySelector('.display');
-    display.textContent = displayText;
+    displayUpdate()
 }
 
 function isPower(x) {
@@ -139,6 +151,42 @@ function solve(exp) {
     return exp
 }
 
+function reduceParentheses() {
+    let inputString = input.join(',');
+    if (!inputString.includes(')') && !inputString.includes('(')) return
+    if (!inputString.includes(')') || !inputString.includes('(')) return 'error'
+    while (inputString.includes('(')) {
+        let indexOpen = inputString.indexOf('(')
+        let indexClose = inputString.indexOf(')');
+        if (indexClose == -1) return 'error'
+        let nextOpen = inputString.indexOf('(', indexOpen + 1);
+        while (nextOpen !== -1) {
+            indexOpen = nextOpen;
+            nextOpen = inputString.indexOf('(', indexOpen + 1)
+        }
+        if (indexClose < indexOpen) return 'error'
+        let expression = inputString.slice(indexOpen + 2, indexClose - 1).split(',');
+        if (expression.includes('(') || expression.includes(')')) return 'error'
+        let answer = solve(expression);
+        inputUpdate(indexOpen, indexClose, answer);
+        inputString = input.join(',')
+    }
+}
+
+function round(answer) {
+    answer = answer.slice(0, 18);
+    if (answer.includes('.')) {
+        let decimalPlaces = 15 - answer.indexOf('.');
+        answer = (Math.round(Number(answer) * 10**decimalPlaces) / 10**decimalPlaces).toString()
+    }
+    else {
+        let decimalPlaces = (answer.length - 1) - 15;
+        answer = (Math.round(Number(answer) / 10**decimalPlaces) * 10**decimalPlaces).toString()
+    }
+    answer = answer.slice(0, 16);
+    return answer
+}
+
 function calculate() {
     const display = document.querySelector('.display')
     const clearEntry = document.querySelector('#clear-entry');
@@ -153,42 +201,42 @@ function calculate() {
             return 'error'
             }
     })
-    let inputString = input.join(',');
-    while (input.some(isOperator) && display.textContent !== 'error')
-        if (input.includes('(')) {
-            inputString = input.join(',');
-            let start = inputString.indexOf('(')
-            let end = inputString.indexOf(')');
-            let smallString = inputString.slice(start + 2, end - 1);
-            let smallInput = smallString.split(',');
-            smallInput = solve(smallInput);
-            let expression = inputString.slice(start, end + 1)
-            smallString = smallInput.join(',');
-            inputString = inputString.replace(expression, smallString);
-            input = inputString.split(',')
-        } 
-        else {
-            input = solve(input);
-        }
+    let reduced;
+    if (input.join('').includes('(')) {
+        reduced = reduceParentheses()
+    }
+    if (reduced == 'error') {
+        display.textContent = 'error'
+        return 'error'
+    }
+    if (input.length > 1) input = solve(input);
     if (display.textContent === 'error') return 'error'
     let answer = input.join('');
     if (answer.length > 16) {
-        if (answer[15] !== '.' && answer[16] !== '.') {
-            let lastNumber = +answer[14]
-            let nextNumber = +answer[15];
-            if (nextNumber > 4) {
-                ++lastNumber;
-                answer = answer.slice(0, 15) + lastNumber;
-            }
-        }
-        else if (answer[16] === '.') {
-            answer = answer.slice(0, 15)
-        }
-        else {
-            answer = answer.slice(0, 16)
-        }
+        round(answer)
     } 
     display.textContent = answer
+}
+
+function clear() {
+    let display = document.querySelector('.display')
+    if (display.textContent.length == 1) {
+        input = ['0']
+        display.textContent = '0'
+        return
+    }
+    let lastIndex = input.length - 1;
+    let lastItem = input[lastIndex];
+    if (lastItem.length > 1 && lastItem != '**' && lastItem != 'sqrt') {
+        let updatedItem = lastItem.slice(0, lastItem.length - 1);
+        input[lastIndex] = updatedItem
+    } else input.pop();
+    displayUpdate()
+}
+
+function type(e) {
+    const button = document.querySelector(`.button[data-key=${e.code}]`);
+    if (button !== null) return button.click()
 }
 
 function activateInputButtons() {
@@ -198,35 +246,21 @@ function activateInputButtons() {
 })
 }
 
-const equalButton = document.querySelector('#equals');
-equalButton.addEventListener('click', calculate)
-
-function clear() {
-    let lastEntry = displayText.length - 1;
-    if (displayText[lastEntry] === ' ') lastEntry = lastEntry - 2;
-    displayText = displayText.slice(0, lastEntry);
-    document.querySelector('.display').textContent = displayText;
-    let inputString = displayText.replaceAll(' × ', ' * ').replaceAll(' ÷ ', ' / ');
-    input = inputString.split(' ')
-}
-
 function activateClearEntry() {
     const clearEntry = document.querySelector('#clear-entry');
     clearEntry.addEventListener('click', clear)
 }
 
+const equalButton = document.querySelector('#equals');
+equalButton.addEventListener('click', calculate)
+
 const clearAll = document.querySelector('#clear-all');
 clearAll.addEventListener('click', function() {
     document.querySelector('.display').textContent = '0';
-    input = [];
+    input = ['0'];
     activateClearEntry();
     activateInputButtons()
 })
-
-function type(e) {
-    const button = document.querySelector(`.button[data-key=${e.code}]`);
-    if (button !== null) return button.click()
-}
 
 window.addEventListener('keydown', type)
 
